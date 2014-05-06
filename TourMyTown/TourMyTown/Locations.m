e5189c6bef007b30805d43034434d43f09f2a40c
lephuocdai <lephuocdaijapan@gmail.com>
2014-05-07 00:52:11 +0900

    Implement import and persist methods

--- TourMyTown/TourMyTown/Locations.m
+++ TourMyTown/TourMyTown/Locations.m
@@ -9,7 +9,8 @@
 #import "Locations.h"
 #import "Location.h"
 
-static NSString* const kBaseURL = @"http://localhost:3000/";
+//static NSString* const kBaseURL = @"http://localhost:3000/";
+static NSString* const kBaseURL = @"lephuocdaiMacBookPro.local";
 static NSString* const kLocations = @"locations";
 static NSString* const kFiles = @"files";
 
@@ -41,10 +42,34 @@ static NSString* const kFiles = @"files";
 }
 
 - (void)parseAndAddLocations:(NSArray*)locations toArray:(NSMutableArray*)destinationArray {
+    for (NSDictionary *item in locations) {
+        Location *location = [[Location alloc] initWithDictionary:item];
+        [destinationArray addObject:location];
+    }
+    if (self.delegate)
+        [self.delegate modelUpdated];
 }
 
 - (void)import {
- 
+    // Set the GET request
+    NSURL *url = [NSURL URLWithString:[kBaseURL stringByAppendingPathComponent:kLocations]];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    request.HTTPMethod = @"GET";
+    [request addValue:@"application/json" forHTTPHeaderField:@"Accept"];
+    
+    // Create session
+    NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:config];
+    
+    // Create task to get data then parse it to an array of Location
+    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if (error == nil) {
+            NSArray *responseArray = [NSJSONSerialization JSONObjectWithData:data options:0 error:NULL];
+            [self parseAndAddLocations:responseArray toArray:_objects];
+        }
+    }];
+    
+    [dataTask resume];  // Start the task
 }
 
 - (void) runQuery:(NSString *)queryString {
@@ -55,8 +80,55 @@ static NSString* const kFiles = @"files";
     
 }
 
-
 - (void) persist:(Location*)location{
- 
+    // Input safety check
+    if (!location || location.name == nil || location.name.length == 0) {
+        return;
+    }
+    
+    // Set the URLString for the collection, in this case it is /locations
+    NSString *collectionURLString = [kBaseURL stringByAppendingPathComponent:kLocations];
+    
+    // Route to /locations endpoint for new location which doesn't have an _id
+    // Instead route to /locations/_id for existing location
+    BOOL isExistingLocation = location._id != nil;
+    NSURL *url = (isExistingLocation) ? [NSURL URLWithString:[collectionURLString stringByAppendingPathComponent:location._id]] :
+    [NSURL URLWithString:collectionURLString];
+    
+    // Set the PUT or POST request with body and header
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    request.HTTPMethod = (isExistingLocation) ? @"PUT" : @"POST";
+    request.HTTPBody = [NSJSONSerialization dataWithJSONObject:location.toDictionary options:0 error:NULL];
+    [request addValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    
+    // Create sesssion
+    NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:config];
+    
+    // Create data task for the request
+    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if (!error) {
+            NSArray *responseArray = @[[NSJSONSerialization JSONObjectWithData:data options:0 error:NULL]];
+            [self parseAndAddLocations:responseArray toArray:_objects];
+        }
+    }];
+    
+    [dataTask resume];
 }
+
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
